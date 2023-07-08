f63015d67f1760bdad826b750d4eee1af38c25cb
thanujan <thanujant@veracitygp.com>
2023-07-08 15:30:18 +0530

    feat: preprocessing added

--- code_base_II/data/data.py
+++ code_base_II/data/data.py
@@ -1,6 +1,6 @@
 
 from sklearn import set_config
-
+import sklearn
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.base import BaseEstimator
@@ -10,107 +10,241 @@ from sklearn.preprocessing import StandardScaler, OneHotEncoder, FunctionTransfo
 
 from sklearn import config_context
 
-"""_summary_
-"""
+from preprocessors import  TypePreprocessor
+
+
 class Data:
+    
+    """
+    A class that represents data and provides methods for preprocessing.
+
+    Attributes:
+        _raw_data (pandas.DataFrame): The raw data.
+        _preprocessed_data (pandas.DataFrame): The preprocessed data.
+        preprocessor: The data preprocessor.
+        _categorical_features (list): List of categorical feature names.
+        _numerical_features (list): List of numerical feature names.
+        _date_features (list): List of date feature names.
+    """
+    
+    
     def __init__(self , raw_data ):
+        """
+        Initializes a new instance of the Data class.
+
+        Args:
+            _raw_data (pandas.DataFrame): The raw data.
+            _preprocessed data (pandas.DataFrame): The preprocessed data.
+            _preprocessor (Preprocessor): These are the functions used to modify the data.
+            _categorical_features = features that have categorical values
+            _numerical_features = features that have numerical values
+            _date_features = features that have date values
+            
+        """
+        
         self._raw_data = raw_data
         self._preprocessed_data = raw_data
-        self.preprocessor = None
+        self._preprocessors = None
+        
         self._categorical_features = None
         self._numerical_features = None
         self._date_features = None
+        self._clean_features = None
     
     
     
     def set_feature_types(self, type_of_features):
         
-        self._categorical_features = type_of_features["categorical_features"]
-        self._numerical_features = type_of_features["numeric_features"]
-        self._date_features = type_of_features["date_features"]
+        """
+        Sets the feature types for the data.
+
+        Args:
+            type_of_features (dict): A dictionary containing the feature types.
+
+        Example:
+            type_of_features = {
+                "categorical_features": ["feature1", "feature2"],
+                "numeric_features": ["feature3", "feature4"],
+                "date_features": ["feature5", "feature6"]
+            }
+            data.set_feature_types(type_of_features)
+        """
+        self._categorical_features = type_of_features[type_of_features["Type"]=="categorical"]["Feature"].values
+        self._numerical_features = type_of_features[type_of_features["Type"]=="date"]["Feature"].values
+        self._date_features = type_of_features[type_of_features["Type"]=="numerical"]["Feature"].values
+        ##print(self._categorical_features)
         return
-        
-        
-        
-    def _load_data(self, file_path, rows):
-        return pd.read_csv(file_path, nrows=100)
     
+    def set_clean_features(self, clean_features):
+        
+        self._clean_features = clean_features
+        
+        return
     
-    def set_preprocessor(self, preprocessor):
-        self.preprocessor = preprocessor 
+    def set_preprocessors(self, preprocessors):
+        """
+        Sets the data preprocessor.
+
+        Args:
+            preprocessor: The preprocessor object.
+        """
+        self._preprocessors = preprocessors
         
     def preprocess_data(self):
-        if(self.preprocessor is None):
-            raise ValueError("Preprocessor must be set")
-                
+        """
+        Preprocesses the data using the set preprocessor.
+
+        Raises:
+            ValueError: If the preprocessor is not set.
+        """
         
+        if(self._preprocessors is None):
+            raise ValueError("Preprocessor must be set")
         # for preprocess in self.preprocessors:
-        with config_context(transform_output="pandas"):
-            self._preprocessed_data = self.preprocessor.fit_transform(self._preprocessed_data)
+        # with sklearn.config_context(transform_output="pandas"):
+        # with config_context(transform_output="pandas"):
+        
+        for preprocessor in self._preprocessors:
+            if(preprocessor.__name__ == "FeatureFilter"):
+                ##print(preprocessor)
+                self._preprocessed_data = preprocessor().preprocess(self._preprocessed_data, self._clean_features)
+            elif(preprocessor.__name__ == "TypePreprocessor"):
+                ##print("hello",self._categorical_features)
+                self._preprocessed_data = preprocessor().preprocess(self._preprocessed_data, self.get_feature_types())
+            # self._preprocessed_data = preprocessor.preprocess(self._preprocessed_data, self._clean_features)
+            # self._preprocessed_data = preprocessor.preprocess(self._preprocessed_data, self._clean_features)
+       
+        # self._preprocessed_data = self._preprocessor.fit_transform(self._preprocessed_data)
+        
+    def get_feature_types(self):
+        """
+        Identifies the feature types in the raw data and sets them in the Data object.
+
+        Returns:
+            self: Returns the instance of the Data object with the feature types set.
+        Raises:
+            ValueError: Raised when the raw data is not set.
+            Exception: Raised when a feature has an unsupported data type.
+
+        This method identifies the feature types (numerical, categorical, or date) in the raw data and sets them in the Data object.
+        It first checks if the raw data is set; if not, a ValueError is raised.
+        The feature types are stored in a pandas DataFrame with columns for 'Feature', 'Type', and 'Format'.
+        The method iterates over each column in the raw data and determines its data type.
+        - If the column's data type is 'float64' or 'int64', it is considered a numerical feature.
+        - If the column's data type is 'object', it attempts to parse the column as a date using the 'MM/DD/YYYY' format.
+        If the parsing is successful, the column is considered a date feature; otherwise, it is considered a categorical feature.
+        - If a column has any other data type, an exception is raised.
+        The method sets the feature types in the Data object using the 'set_feature_types' method.
+        The Data object should have a 'set_feature_types' method to receive and store the feature types.
+        The method returns the updated Data object.
+
+        Example:
+            data.add_feature_types()
+        """
+        
+        if self._preprocessed_data is None:
+            raise ValueError("raw data must be set")
         
 
+        records = []
+        for column in self._preprocessed_data.columns:
+            column_type = self._preprocessed_data[column].dtype
+
+            if column_type in ['float64', 'int64']:
+                record = {'Feature': column, 'Type': 'numerical', 'Format': ''}
+                
+            elif column_type == 'object':
+                try:
+                    # Attempt to parse the column as a date
+                    parse(self._preprocessed_data[column].iloc[0], dayfirst=True)
+                    record = {'Feature': column, 'Type': 'date', 'Format': 'MM/DD/YYYY'}
+                except ValueError:
+                    record = {'Feature': column, 'Type': 'categorical', 'Format': ''}
+            else:
+                raise Exception(f"Feature ({column} : {self._preprocessed_data[column].iloc[0]}) is not among float64|int64|string|datetime", )
+            records.append( record )
+            
+        feature_types = pd.DataFrame.from_records(records,columns=['Feature', 'Type', 'Format'])
         
-        
+        return feature_types 
+      
     def get_data(self):
-        return self._raw_data
+        """
+        Returns the raw data.
+
+        Returns:
+            pandas.DataFrame: The raw data.
+        """
+        return self._preprocessed_data
         
     def get_preprocessed_data(self):
+        """
+        Returns the preprocessed data.
+
+        Returns:
+            pandas.DataFrame: The preprocessed data.
+        """
+        
         return self._preprocessed_data 
     
-    # def set_feature_types_auto(self):
-        
-    #     type_of_features = {
-    #         "numeric_features" : [],
-    #         "categorical_features" : [],
-    #         "date_features" : []
-    #     }
-        
-    #     for column in self.data.get_data().columns:
-            
-    #         if ('date' in column.lower):
-    #             type_of_features["date_features"].append(column)
-                
-    #         elif (self.data.get_data()[column].dtype in ["float64", "int64"]):
-    #             type_of_features["numeric_features"].append(column)
-                
-    #         elif (self.data.get_data()[column].dtype == 'object'):
-    #             type_of_features["categorical_features"].append(column)
-        
-    #     self.data.set_feature_types(type_of_features)
-        
-    #     return 
 
-
+from dateutil.parser import parse
     
 class DataBuilder:
     
     def __init__(self):
-        self.file_path = None
+        self._raw_data = None
         self.preprocessors = []
+        self.feature_types = None
+        self.clean_features = None
         
-    def set_file_path(self, file_path):
-        self.file_path = file_path
+    def _load_data(self, file_path, rows):
+        return pd.read_csv(file_path, nrows=rows)
+        
+    def load_data(self, file_path, rows):
+        self._raw_data = self._load_data(file_path, rows)
         return self
     
     def add_preprocessor(self, preprocessor):
         self.preprocessors.append(preprocessor)
         return self
     
+    
+    def add_clean_features(self, clean):
+        
+        self.clean_features = clean
+        
+        return self
+
+        
+        
+        
+    
     def build(self):
-        if self.file_path is None or not self.preprocessors:
-            raise ValueError("File path and preprocessor must be set")
+        if self._raw_data is None or not self.preprocessors:
+            raise ValueError("raw data and preprocessor must be set")
         
-        raw_data = pd.read_csv(self.file_path)
         
-        data = Data(raw_data.copy())
         
-        preprocessor = ColumnTransformer(transformers=[(str(type(preprocessor).__name__), preprocessor, list(raw_data.columns)) for i, preprocessor in enumerate(self.preprocessors)], remainder= "passthrough")
+        data = Data(self._raw_data.copy())
+        
+        
+        # ##print(self._raw_data.columns)
+       
+        # preprocessor = ColumnTransformer(transformers=[(str(type(preprocessor).__name__), preprocessor, []) for i, preprocessor in enumerate(self.preprocessors)], remainder= "passthrough")
+
+        
+        
+        data.set_preprocessors(self.preprocessors)
+        
  
-        data.set_preprocessor(preprocessor)
+        # data.set_feature_types(self.feature_types)
+        
+        data.set_clean_features(self.clean_features)
         
         return data
     
-    
+
     
 from feature_filter import FeatureFilter
     
@@ -126,14 +260,14 @@ if __name__ == "__main__":
     # preprocessor = data.with_feature_filter(("feture_detect",FeatureFilter(["Project ID"]))).build()
     
     data = DataBuilder()\
-        .set_file_path("./test.csv")\
-        .add_preprocessor(FeatureFilter(['ID',"Unnamed:"]))\
+        .load_data("./test.csv",100)\
+        .add_clean_features(['ID',"Unnamed:"])\
+        .add_preprocessor(FeatureFilter)\
+        .add_preprocessor(TypePreprocessor)\
         .build()
-
-    data.preprocess_data()
-    
-    
-    
         
-    print(data.get_preprocessed_data())
-    
+        
+        
+    data.preprocess_data()
+    print(data.get_preprocessed_data().head())
+   
--- code_base_II/data/preprocessors.py
+++ code_base_II/data/preprocessors.py
@@ -1,25 +1,48 @@
+from abc import ABC, abstractmethod
+    
+
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
 import pandas as pd
 
-class NumericPreprocessor:
-    def __init__(self, numeric_features):
-        self.numeric_features = numeric_features
-        self.scaler = StandardScaler()
-    
+class Preprocessor(ABC):
+    @abstractmethod
     def preprocess(self, data):
-        numeric_data = data[self.numeric_features]
-        scaled_data = self.scaler.fit_transform(numeric_data)
-        scaled_data = pd.DataFrame(scaled_data, columns=self.numeric_features)
-        return scaled_data
+        pass
+    
 
-class CategoricalPreprocessor:
-    def __init__(self, categorical_features):
-        self.categorical_features = categorical_features
+class TypePreprocessor(Preprocessor):
+    def __init__(self):
+        # self.categorical_features = 
         self.encoder = OneHotEncoder()
-    
-    def preprocess(self, data):
-        categorical_data = data[self.categorical_features]
+        self.scaler = StandardScaler()
+       
+    # data should be 
+    def preprocess(self, data: pd.DataFrame, feature_types):
+        
+        
+        numeric_data = data[feature_types[feature_types["Type"]=="numerical"]["Feature"]]
+        scaled_data = self.scaler.fit_transform(numeric_data)
+        scaled_data = pd.DataFrame(scaled_data, columns=numeric_data.columns)
+        
+        
+        date_data = data[feature_types[feature_types["Type"]=="date"]["Feature"]].copy()
+        
+        # for column in date_data.columns:
+        #     date_data[column] = pd.to_datetime(date_data[column])
+        #     date_data[column + '_Year'] = date_data[column].dt.year
+        #     date_data[column + '_Month'] = date_data[column].dt.month
+        #     date_data[column + '_Day'] = date_data[column].dt.day
+        #     date_data = date_data.drop(column,axis=1)
+        for column in date_data.columns:
+            date_data[column] = pd.to_datetime(date_data[column])
+        
+        categorical_data = data[feature_types[feature_types["Type"]=="categorical"]["Feature"]]
         encoded_data = self.encoder.fit_transform(categorical_data).toarray()
-        encoded_columns = self.encoder.get_feature_names_out(self.categorical_features)
+        encoded_columns = self.encoder.get_feature_names_out(categorical_data.columns)
         encoded_data = pd.DataFrame(encoded_data, columns=encoded_columns)
-        return encoded_data
+        
+        preprocessed_data = pd.concat([date_data, scaled_data,encoded_data],axis=1)
+    
+        return preprocessed_data
+
+
