3a533056529f2a5b95ec4b48b2df99738445210b
DrMadhushan <dev.drmad@gmail.com>
2023-07-13 19:23:57 +0530

    add Desicion Tree with max 3 levels & 500 trees in RF

--- code/main.py
+++ code/main.py
@@ -4,6 +4,7 @@ import seaborn as sns
 from datetime import timedelta
 import matplotlib.pyplot as plt
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.tree import DecisionTreeClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import precision_score, recall_score, accuracy_score
@@ -48,7 +49,7 @@ dp.export_data_frame(data=data, path=file_path)
 classifier_1 = LogisticRegression()
 parameters_1 = {"penalty":["l1","l2"]}
 
-classifier_2 = RandomForestClassifier()
+classifier_2 = RandomForestClassifier(n_estimators=500)
 # parameters_2 = {'max_depth':[2, 3, 4], 
 #                 'n_estimators':[5, 10, 20], 
 #                 'min_samples_split': 2, 
@@ -56,9 +57,25 @@ classifier_2 = RandomForestClassifier()
 
 # classifier_3 = svm.SVC(kernel='linear')
 
-data = dp.encode_data(data, config.CATEGORICAL_COLS)
-print("encoded_data.shape = ", data.shape)
+classifier_4 = DecisionTreeClassifier(max_depth=3)
 
-trained_model, eval_metrics, avg_metrics = fe.run_pipeline(data, classifier_2)
-fe.plot_k_fold_evaluation_metrics(eval_metrics)
+data_1 = dp.encode_data(data, config.CATEGORICAL_COLS)
+print("encoded_data.shape = ", data_1.shape)
+
+data_2 = data_1.copy(deep=True)
+data_3 = data_1.copy(deep=True)
+
+print("Classifier: Logistic Regression")
+trained_model, eval_metrics, avg_metrics = fe.run_pipeline(data_1, classifier_1, 'log_reg/')
+fe.plot_k_fold_evaluation_metrics(eval_metrics, 'log_reg/')
 save_model(file_name=f'LogReg_{str(time.strftime("%Y-%m-%d %H:%M:%S"))[:10]}.sav', model=trained_model)
+
+# print("Classifier: Random Forest")
+# trained_model, eval_metrics, avg_metrics = fe.run_pipeline(data_1, classifier_2, 'random_forest/')
+# fe.plot_k_fold_evaluation_metrics(eval_metrics, 'random_forest/')
+# save_model(file_name=f'RandForest_{str(time.strftime("%Y-%m-%d %H:%M:%S"))[:10]}.sav', model=trained_model)
+
+# print("Classifier: Decision Tree")
+# trained_model, eval_metrics, avg_metrics = fe.run_pipeline(data_3, classifier_4, 'decision_tree/')
+# fe.plot_k_fold_evaluation_metrics(eval_metrics, 'decision_tree/')
+# save_model(file_name=f'DecTree_{str(time.strftime("%Y-%m-%d %H:%M:%S"))[:10]}.sav', model=trained_model)
