afff3d23ff8100d5d2175ede30a1b2f89a6c1618
RavishaHR <e17296@eng.pdn.ac.lk>
2023-08-08 09:54:02 +0530

    adding the treeshap implementation

--- code/shap.py
+++ code/shap.py
@@ -16,6 +16,11 @@ def get_shap_explanation(x_train, x_test, instance_loc, model, model_name):
     # Can be either 0 or 1 for binary classification
     #shap.force_plot(explainer_shap.expected_value[0], shap_values[0], instance)
     filepath = f'{config.SHAP_DEST}shap_exp_{instance_loc}_{model_name}.png'
-    shap.force_plot(explainer_shap.expected_value[0], shap_values[0], instance, show=False, matplotlib=True, text_rotation=45).savefig(filepath, format = "png",dpi = 150, bbox_inches = 'tight') 
+    shap.force_plot(explainer_shap.expected_value[0], 
+                    shap_values[0], 
+                    instance, 
+                    show=False, 
+                    matplotlib=True, 
+                    text_rotation=45).savefig(filepath, format = "png", dpi = 150, bbox_inches = 'tight') 
 
     return shap_values
