28e1dc2d244ccf1cd631f8aef467c3890b31ceac
thanujan <thanujant@veracitygp.com>
2023-08-16 10:43:49 +0530

    bug: tree shape

--- code/treeshap.py
+++ code/treeshap.py
@@ -1,24 +1,41 @@
 import shap
 import config
 
-def get_treeshap_explanation(x_train, x_test, instance_loc, model, model_name):
+def get_treeshap_explanation(x_train, x_test,  top_instance_loc_list, bottom_instance_loc_list, model, model_name):
 
     #shap.initjs()
     
     # Define the KernelSHAP explainer
     explainer_tree = shap.TreeExplainer(model=model, data=x_train, model_output="raw")
 
-    # Explain for the selected instance
-    instance = x_test.iloc[instance_loc]
-    treeshap_values = explainer_tree.shap_values(instance)
+    for instance_loc in top_instance_loc_list:
+        # Explain for the selected instance
+        instance = x_test.iloc[instance_loc]
+        treeshap_values = explainer_tree.shap_values(instance)
 
-    # Visualize and save
-    filepath = f'{config.TREESHAP_DEST}treeshap_exp_{instance_loc}_{model_name}.png'
-    shap.force_plot(explainer_tree.expected_value[0], 
-                treeshap_values[0],
-                instance,
-                show=False, 
-                matplotlib=True, 
-                text_rotation=45).savefig(filepath, format = "png", dpi = 150, bbox_inches = 'tight')
+        # Visualize and save
+        filepath = f'{config.TREESHAP_DEST}top/treeshap_exp_{instance_loc}_{model_name}.png'
+        shap.force_plot(explainer_tree.expected_value[0], 
+                    treeshap_values[0],
+                    instance,
+                    show=False, 
+                    matplotlib=True, 
+                    text_rotation=45).savefig(filepath, format = "png", dpi = 150, bbox_inches = 'tight')
+        
+        # return treeshap_values
     
-    return treeshap_values
+    for instance_loc in bottom_instance_loc_list:
+        # Explain for the selected instance
+        instance = x_test.iloc[instance_loc]
+        treeshap_values = explainer_tree.shap_values(instance)
+
+        # Visualize and save
+        filepath = f'{config.TREESHAP_DEST}bottom/treeshap_exp_{instance_loc}_{model_name}.png'
+        shap.force_plot(explainer_tree.expected_value[0], 
+                    treeshap_values[0],
+                    instance,
+                    show=False, 
+                    matplotlib=True, 
+                    text_rotation=45).savefig(filepath, format = "png", dpi = 150, bbox_inches = 'tight')
+        
+        # return treeshap_values
