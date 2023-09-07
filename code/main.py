9e3129b610e30dc048a12bce1682f4ed36bd53dc
DrMadhushan <dev.drmad@gmail.com>
2023-09-07 14:05:46 +0530

    add 2000 trees to RF

--- code/main.py
+++ code/main.py
@@ -18,7 +18,7 @@ load_processed_data = config.LOAD_PROCESSED_DATA_FLAG
 
 # create classifiers including baseline models
 rand_for_params = create_random_forest_parameters(
-    max_depths=[4], n_estimators=[200, 500])
+    max_depths=[4], n_estimators=[200, 2000])
 # log_reg_params = create_logistic_regression_parameters(
 #     max_iters=[100], penalties=["l1"])
 # xgb_classifier_params = create_xgb_classifier_parameters()
