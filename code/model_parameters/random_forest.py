74c90a4859b77c9e2f75b4a2363cae981a64a505
DrMadhushan <dev.drmad@gmail.com>
2023-09-18 09:10:29 +0530

    add more hyperparameters to rf

--- code/model_parameters/random_forest.py
+++ code/model_parameters/random_forest.py
@@ -13,4 +13,18 @@ rf_parameters = [
         'min_samples_split': 2,
         'min_samples_leaf': 2
     },
+    {
+        'criterion': "gini",
+        'max_depth': 10,
+        'n_estimators': 1000,
+        'min_samples_split': 2,
+        'min_samples_leaf': 2
+    },
+    {
+        'criterion': "gini",
+        'max_depth': 10,
+        'n_estimators': 5000,
+        'min_samples_split': 2,
+        'min_samples_leaf': 2
+    },
 ]
