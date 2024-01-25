c1d0c5ee79ffd7df70e61fea26ea9798c484c4b4
DrMadhushan <dev.drmad@gmail.com>
2024-01-25 18:52:09 +0530

    update sp char removal

--- code/config.py
+++ code/config.py
@@ -25,7 +25,7 @@ PROCESSED_DATA_PATH = DATA_DEST + "processed_final_data_latest.csv"
 PROGRAM_LOG_FILE = "program_log.log"
 
 LOAD_PROCESSED_DATA_FLAG = True
-REMOVE_SP_CH_FROM_FEATURE_NAMES = True
+REMOVE_SP_CH_FROM_FEATURE_NAMES = False
 MAX_ROWS = 500000  # not in use now (18/08/2023)
 
 FIXED_KVAL = 1000
